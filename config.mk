809f0399b6d33932d6aab7ea0ab5a8d0a9c9c562
Csorian <inbox@csorian.uk>
2021-03-28 21:56:49 +0100

    Set $HOME/.local prefixes.

--- config.mk
+++ config.mk
@@ -2,8 +2,8 @@
 VERSION = 5.0
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+PREFIX = ${HOME}/.local
+MANPREFIX = $(PREFIX)/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
